b73a1b860497888a650b9b26f5bf9ff0c0eb8998
Karsten Kirby Uy <karsten.uy@gmail.com>
2024-01-04 15:02:08 -0800

    Some footer prep work

--- karsten_website/src/components/Footer.jsx
+++ karsten_website/src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import styles from "../style";
+import {socialMedia} from "../constants"
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <section className={'${styles.flexCenter} ${styles.paddingY} flex-col'}>
+    <div className={'${styles.flexStart} md:flex-row flex-col mb-8 w-full'}>
+
+    </div>
+  </section>
+)
+
 
 export default Footer
