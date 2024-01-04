52cd94fc0d427ddeb9bc37f5cc6d7f616ae31868
Karsten Kirby Uy <karsten.uy@gmail.com>
2024-01-04 14:50:33 -0800

    home block done

--- karsten_website/src/components/CentreBlock.jsx
+++ karsten_website/src/components/CentreBlock.jsx
@@ -4,19 +4,20 @@ import { home_description_block } from "../constants";
 const CentreBlock = () => (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className="flex flex-row items-center py-[60px] px-[60px] bg-discount-gradient rounded-[70px] mb-2">
+      <div className="flex flex-row items-center py-[60px] px-[60px] bg-discount-gradient rounded-[10px]">
         {home_description_block.map((block) => (
-          <div key={block.id} className="flex items-center">
+          <div key={block.id} className="flex items-center md:flex-row flex-col">
             <img
               src={block.img}
               alt="Description"
-              style={{ width: '600px', height: '600px', marginRight: '20px' }} // Adjust the width, height, and margin as needed
+              className="w-full max-w-[600px] h-auto md:max-w-full md:w-auto md:mr-4" // Make the image responsive
             />
-            <p className="font-poppins font-normal text-[35px] text-white ml-10" >{block.content}</p>
+            <p className="font-poppins font-normal text-white mt-4 sm:mt-0 md:ml-10 md:text-[35px] text-[20px]">{block.content}</p>
           </div>
         ))}
       </div>
     </div>
   </section>
 );
+
 export default CentreBlock;
