b73a2c967f87351c8522ba651b440496dd292a37
Karsten Kirby Uy <karsten.uy@gmail.com>
2024-01-04 15:50:36 -0800

    Footer Done

--- karsten_website/src/components/Footer.jsx
+++ karsten_website/src/components/Footer.jsx
@@ -1,13 +1,20 @@
 import styles from "../style";
-import {socialMedia} from "../constants"
+import { socialMedia } from "../constants";
 
 const Footer = () => (
-  <section className={'${styles.flexCenter} ${styles.paddingY} flex-col'}>
-    <div className={'${styles.flexStart} md:flex-row flex-col mb-8 w-full'}>
-
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className="flex flex-row md:mt-0 mt-6">
+      {socialMedia.map((social, index) => (
+        <a key={social.id} href={social.link} target="_blank" rel="noopener noreferrer">
+          <img
+            src={social.icon}
+            alt={social.link}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
+          />
+        </a>
+      ))}
     </div>
   </section>
-)
+);
 
-
-export default Footer
+export default Footer;
